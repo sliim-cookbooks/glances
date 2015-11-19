92b433b2915dd49f0138ab814051c6e05d8e801f
Sliim <sliim@mailoo.org>
2015-11-20 00:49:55 +0100

    Fixed monkeypatch
    Don't use `super` method and copied original
    `Kitchen::Driver::Docker#generate_keys` source code from
    `kitchen-docker` version `2.3.0`

--- test/kitchen-docker.rb
+++ test/kitchen-docker.rb
@@ -4,7 +4,19 @@ module Kitchen
     class Docker < Kitchen::Driver::SSHBase
       def generate_keys
         return unless config[:public_key] && config[:private_key]
-        super
+        if !File.exist?(config[:public_key]) || !File.exist?(config[:private_key])
+          private_key = OpenSSL::PKey::RSA.new(2048)
+          blobbed_key = Base64.encode64(private_key.to_blob).gsub("\n", '')
+          public_key = "ssh-rsa #{blobbed_key} kitchen_docker_key"
+          File.open(config[:private_key], 'w') do |file|
+            file.write(private_key)
+            file.chmod(0600)
+          end
+          File.open(config[:public_key], 'w') do |file|
+            file.write(public_key)
+            file.chmod(0600)
+          end
+        end
       end
     end
   end
